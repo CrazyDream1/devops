cd189b50c0104e1f3ce2559012c0ae0e62ab9b20
CrazyDream1 <pavlov.sl2000@mail.ru>
2021-09-01 00:20:52 +0300

    Update test.py

--- app_python/test.py
+++ app_python/test.py
@@ -12,15 +12,15 @@ class TestState(unittest.TestCase):
         current_time = datetime.datetime.now(tz=timezone).strftime('%Y-%m-%d %H:%M:%S')
         self.assertEqual(self.app.index(), current_time)
         
-    def test_check_time_difference_dynamic(self):
+    #def test_check_time_difference_dynamic(self):
 
-        self.app = HelloWorld()
-        timezone = datetime.timezone(datetime.timedelta(hours=3))
-        current_time1 = datetime.datetime.now(tz=timezone).strftime('%Y-%m-%d %H:%M:%S')
-        time.sleep(2)
-        current_time2 = datetime.datetime.now(tz=timezone).strftime('%Y-%m-%d %H:%M:%S')
-        current_time1 = str(current_time1[:-2]) + str(current_time1[-2:] + 2)
-        self.assertEqual(current_time1, current_time2)
+    #    self.app = HelloWorld()
+    #    timezone = datetime.timezone(datetime.timedelta(hours=3))
+    #    current_time1 = datetime.datetime.now(tz=timezone).strftime('%Y-%m-%d %H:%M:%S')
+    #    time.sleep(2)
+    #    current_time2 = datetime.datetime.now(tz=timezone).strftime('%Y-%m-%d %H:%M:%S')
+    #    current_time1 = str(current_time1[:-2]) + str(current_time1[-2:] + 2)
+    #    self.assertEqual(current_time1, current_time2)
 
 if __name__ == "__main__":
     unittest.main()
